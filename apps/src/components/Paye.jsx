e1e61faf07a6bca2e741f517276f6a74e1ee027a
rolissecodeur <rolissecodeur@gmail.com>
2025-11-03 17:25:26 +0100

    Feacture : FedaPaye ajouté pour les abonnements

--- apps/src/components/Paye.jsx
+++ apps/src/components/Paye.jsx
@@ -1,12 +1,14 @@
 import { useMutation } from "@tanstack/react-query";
-// import { FedaCheckoutButton } from "fedapay-reactjs";
+import { FedaCheckoutButton } from "fedapay-reactjs";
 import PropTypes from "prop-types";
 import toast from "react-hot-toast";
 import axiosInstance from "../config/axiosConfig";
 import { useAuth } from "../context/AuthContext";
 
 const Paye = ({ packId, montant }) => {
-  const PUBLIC_KEY = "pk_sandbox_JXVRe2Q6LYWAxqt6MjEgW41F";
+  const PUBLIC_KEY = import.meta.env.VITE_FEDA_PUBLIC_SANDBOX_KEY;
+  //const PUBLIC_KEY = import.meta.env.VITE_FEDA_PUBLIC_LIVE_KEY;
+  
   const { user } = useAuth();
 
   const { mutate: createOrRenewAbonnement } = useMutation({
@@ -16,6 +18,8 @@ const Paye = ({ packId, montant }) => {
         data
       ),
     onSuccess: (response) => {
+      console.log(response);
+      
       toast.success(response?.data?.message || "Paiement et renouvellement effectués avec succès !", {
         duration: 3000,
       });
@@ -29,7 +33,6 @@ const Paye = ({ packId, montant }) => {
     },
   });
 
-  // eslint-disable-next-line no-unused-vars
   const checkoutButtonOptions = {
     public_key: PUBLIC_KEY,
     transaction: {
@@ -43,21 +46,27 @@ const Paye = ({ packId, montant }) => {
       class: "btn btn-primary w-100",
       text: `Choisir ce Pack`,
     },
-    onComplete: (resp) => {
-      const FedaPay = window["FedaPay"];
-      if (resp.reason === FedaPay.COMPLETE) {
-        createOrRenewAbonnement({
-          packId: packId,
-          userId: user.id,
-        });
-      }
-    },
+ onComplete: (resp) => {
+  if (resp.reason === "CHECKOUT COMPLETE" && resp.transaction?.status === "approved") {
+    
+    toast.success("Paiement FedaPay validé, mise à jour en cours...");
+    
+    createOrRenewAbonnement({
+      packId: packId,
+      userId: user.id,
+      transactionId: resp.transaction.id 
+    });
+
+  } else {
+    toast.error("Le paiement a échoué ou a été annulé.");
+  }
+},
   };
 
   return (
     <div>
-      <button className="btn btn-primary" onClick={() => createOrRenewAbonnement({ packId: packId, userId: user.id })}>Renouveler Maintenant</button>
-      {/* <FedaCheckoutButton options={checkoutButtonOptions} /> */}
+      {/* <button className="btn btn-primary" onClick={() => createOrRenewAbonnement({ packId: packId, userId: user.id })}>Renouveler Maintenant</button> */}
+      <FedaCheckoutButton options={checkoutButtonOptions} />
     </div>
   );
 };
