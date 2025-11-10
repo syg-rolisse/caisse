cb349658dab081b9cc8e07bb3621214f762caed0
rolissecodeur <rolissecodeur@gmail.com>
2025-11-10 21:28:44 +0100

    Feact : Gestions de l'impressions des fiches

--- apps/src/utils/downloadDepense.js
+++ apps/src/utils/downloadDepense.js
@@ -8,7 +8,6 @@ const FONT_BOLD = 'Quicksand-Bold';
 
 /**
  * Fonction centrale pour dessiner une fiche de dépense sur un document PDF.
- * (Mise en page latérale pour maximiser deux fiches sur A5)
  */
 export const drawDepenseFiche = (doc, depense, companyInfo, formatDate, yStart) => {
     let y = yStart;
